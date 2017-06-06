a1707bb02859804cb94b649af1ed0f3de95af181
sushant12 <sussyoung9@gmail.com>
2017-06-06 14:02:59 +0545

    disable save btn when task is empty in new task

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,9 @@
-console.log("Namaste");
+function checkEmpty(){
+	var task_length = document.getElementById("new-task-name").value.length;
+	if(task_length > 0)
+	{
+		document.getElementById("save-new-task").removeAttribute("disabled");
+	}else{
+		document.getElementById("save-new-task").setAttribute("disabled","disabled");
+	}
+}
