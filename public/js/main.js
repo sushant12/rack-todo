02673e79d8961f0789c005b4981541f9f9629894
sushant12 <sussyoung9@gmail.com>
2017-06-08 16:56:25 +0545

    refactored js

--- public/js/main.js
+++ public/js/main.js
@@ -29,56 +29,37 @@ Todo.checkEmpty = function (){
 // saves to db
 Todo.makeRequest = function (e) {
     e.preventDefault();
-    var task_name = document.getElementById("new-task-name").value,
-        httpRequest = new XMLHttpRequest();
-    httpRequest.open('POST', '/save', true);
-    httpRequest.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    httpRequest.onreadystatechange = function() {
-        if(httpRequest.readyState == 4 && httpRequest.status == 200) {
-            var res = JSON.parse(httpRequest.response),
-             ul = document.getElementById("task-list"),
-             li = document.createElement("li"),
-             a_edit = document.createElement("a"),
-             a_del = document.createElement("a");
-
-            a_edit.setAttribute('href','/edit/'+res.id);
-            a_edit.setAttribute("class","edit-task");
-            a_edit.addEventListener("click",function(e){
-                Todo.editTask(e);
-            }, false);
-            a_edit.innerHTML = "Edit";
-
-            a_del.setAttribute('href','/delete/'+res.id);
-            a_del.setAttribute("class","del-task");
-            a_del.addEventListener("click",function(e){
-                Todo.deleteTask(e);
-            }, false);
-            a_del.innerHTML = "Delete";
-            li.appendChild(document.createTextNode(res.name+" "));
-            li.appendChild(a_edit);
-            li.appendChild(document.createTextNode(" | "));
-            li.appendChild(a_del);
-            li.setAttribute("id", res.name+"-"+res.id);
-            ul.appendChild(li);
-        }
-    }
-    httpRequest.send("task="+task_name);
+    var task_name = document.getElementById("new-task-name").value;
+        // httpRequest = new XMLHttpRequest();
+    Todo.ajaxify('POST','/save',Todo.saveTaskHTML,"task="+task_name,e);
 }
 
+Todo.saveTaskHTML = function(e, httpRequest){
+    var res = JSON.parse(httpRequest.response),
+        ul = document.getElementById("task-list"),
+        li = document.createElement("li");
+    var a_edit = document.createElement("a"),
+        a_del = document.createElement("a");
+    link_html(e,res,a_edit,a_del);
+    li.appendChild(document.createTextNode(res.name+" "));
+    li.appendChild(a_edit);
+    li.appendChild(document.createTextNode(" | "));
+    li.appendChild(a_del);
+    li.setAttribute("id", res.name+"-"+res.id);
+    ul.appendChild(li);
+}
+
+// deletes task
 Todo.deleteTask = function (e){
 	e.preventDefault();
-	var id = e.currentTarget.href.split("/")[e.currentTarget.href.split("/").length -1],
-	    httpRequest = new XMLHttpRequest();
-	httpRequest.open('POST', '/delete', true);
-    httpRequest.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-	httpRequest.onreadystatechange = function() {
-		if(httpRequest.readyState == 4 && httpRequest.status == 200) {
-			var parent = e.target.parentNode,
-                list = document.getElementById("task-list");
-			list.removeChild(parent);
-		}
-	}
-	httpRequest.send("id="+id);
+	var id = e.currentTarget.href.split("/")[e.currentTarget.href.split("/").length -1];
+    Todo.ajaxify('POST','/delete',Todo.removeTaskHTML,"id="+id,e);
+}
+
+Todo.removeTaskHTML = function(e){
+    var parent = e.target.parentNode,
+        list = document.getElementById("task-list");
+    list.removeChild(parent);
 }
 
 // fetch from db to show value in the form
@@ -86,30 +67,24 @@ Todo.editTask = function (e) {
 	e.preventDefault();
 	var form = document.getElementById("edit-form"),
         id = e.currentTarget.href.split("/")[e.currentTarget.href.split("/").length -1],
-        httpRequest = new XMLHttpRequest(),
-        finished = document.getElementById('finished-task'),
         finished_value = 0;
 	form.style.display = "block";
 	task_ref = e.target.parentElement;
+    Todo.ajaxify('GET', '/edit/'+id,Todo.viewDataInForm ,'',e);
+}
 
-	httpRequest.open('GET', '/edit/'+id, true);
-
-	httpRequest.onreadystatechange = function() {
-		if(httpRequest.readyState == 4 && httpRequest.status == 200) {
-			var res = JSON.parse(httpRequest.response),
-			    task = document.getElementById('edit-task-name');
-			    task.value = res.name;
-			
-			if(res.finished){
-				finished.checked = true;
-			}else{
-				finished.checked = false;
-			}
-			var task_id = document.getElementById('task-id');
-            task_id.value = id;
-		}
-	}
-	httpRequest.send();
+Todo.viewDataInForm = function(e, httpRequest) {
+        var res = JSON.parse(httpRequest.response),
+            finished = document.getElementById('finished-task'),
+            task = document.getElementById('edit-task-name');
+        task.value = res.name;
+        if(res.finished){
+            finished.checked = true;
+        }else{
+            finished.checked = false;
+        }
+        var task_id = document.getElementById('task-id');
+        task_id.value = res.id;
 }
 
 // updates the db
@@ -123,42 +98,53 @@ Todo.updateTask = function (e){
 	if(finished.checked){
 		finished_value = 1;
 	}
+    Todo.ajaxify('POST', '/update/'+id,Todo.updateTaskHTML,"task="+edit_task_name+"&finished="+finished_value,e);
+}
 
-	var httpRequest = new XMLHttpRequest();
-	httpRequest.open('POST', '/update/'+id, true);
-	httpRequest.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-	httpRequest.onreadystatechange = function() {
-		if(httpRequest.readyState == 4 && httpRequest.status == 200) {
-			var res = JSON.parse(httpRequest.response),
-			    a_edit = document.createElement("a"),
-                a_del = document.createElement("a");
+Todo.updateTaskHTML = function(e, httpRequest){
+    var res = JSON.parse(httpRequest.response),
+        a_edit = document.createElement("a"),
+        a_del = document.createElement("a");
+    link_html(e,res,a_edit,a_del);
+    task_ref.innerText = "";
+    task_ref.appendChild(document.createTextNode(res.name+" "));
+    if(res.finished){
+        task_ref.appendChild(document.createTextNode("(Finished)"));
+    }
+    task_ref.appendChild(a_edit);
+    task_ref.appendChild(document.createTextNode(" | "));
+    task_ref.appendChild(a_del);
+    task_ref.setAttribute("id", res.name+"-"+res.id);
+    var form = document.getElementById("edit-form");
+    form.style.display = "none";
+}
 
-            a_edit.setAttribute('href','/edit/'+res.id);
-            a_edit.setAttribute("class","edit-task");
-            a_edit.addEventListener("click",function(e){
-                Todo.editTask(e);
-            }, false);
-             a_edit.innerHTML = "Edit";
+/*
+ * verb shud be POST, GET , DELETE ..
+ */
+Todo.ajaxify = function(verb,url,orsc_func,param,e){
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.open(verb, url, true);
+    httpRequest.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    httpRequest.onreadystatechange = function() {
+        if(httpRequest.readyState == 4 && httpRequest.status == 200) {
+            orsc_func(e,httpRequest);
+        }
+    }
+    httpRequest.send(param);
+}
 
-            a_del.setAttribute('href','/delete/'+res.id);
-            a_del.setAttribute("class","del-task");
-            a_del.addEventListener("click",function(e){
-                Todo.deleteTask(e);
-            }, false);
-            a_del.innerHTML = "Delete";
-
-			task_ref.innerText = "";
-			task_ref.appendChild(document.createTextNode(res.name+" "));
-			if(res.finished){
-				task_ref.appendChild(document.createTextNode("(Finished)"));
-			}
-			task_ref.appendChild(a_edit);
-			task_ref.appendChild(document.createTextNode(" | "));
-			task_ref.appendChild(a_del);
-			task_ref.setAttribute("id", res.name+"-"+res.id); 
-			var form = document.getElementById("edit-form");
-			form.style.display = "none";
-		}
-	}
-	httpRequest.send("task="+edit_task_name+"&finished="+finished_value);
+function link_html(e, res,a_edit,a_del){
+    a_edit.setAttribute('href','/edit/'+res.id);
+    a_edit.setAttribute("class","edit-task");
+    a_edit.addEventListener("click",function(e){
+        Todo.editTask(e);
+    }, false);
+    a_edit.innerHTML = "Edit";
+    a_del.setAttribute('href','/delete/'+res.id);
+    a_del.setAttribute("class","del-task");
+    a_del.addEventListener("click",function(e){
+        Todo.deleteTask(e);
+    }, false);
+    a_del.innerHTML = "Delete";
 }
